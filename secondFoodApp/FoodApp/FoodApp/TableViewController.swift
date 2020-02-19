cb5f3c87b61c66070ecf569c56c6cad37554324c
brcamp13 <brandon.camp.bell@outlook.com>
2020-02-19 11:34:43 -0800

    Finish application

--- secondFoodApp/FoodApp/FoodApp/TableViewController.swift
+++ secondFoodApp/FoodApp/FoodApp/TableViewController.swift
@@ -67,25 +67,22 @@ class TableViewController: UITableViewController {
     }
 
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            // Delete the row from the data source first
+            let row = indexPath.row
+            self.foods.remove(at: row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
