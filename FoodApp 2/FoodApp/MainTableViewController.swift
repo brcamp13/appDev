636069530223f5db1b7cb9e050da9b64178a1917
brcamp13 <brandon.camp.bell@outlook.com>
2020-03-02 14:22:11 -0800

    Implement the alert box and changing cell color

--- FoodApp 2/FoodApp/MainTableViewController.swift
+++ FoodApp 2/FoodApp/MainTableViewController.swift
@@ -13,7 +13,7 @@ class MainTableViewController: UITableViewController {
     var foodItems = [FoodItem]()
 
     @IBAction func addFoodItemTapped(_ sender: UIBarButtonItem) {
-        let foodItem = FoodItem(name: "Food", imageFileName: "food.png", caloriesPerServing: 100)
+        let foodItem = FoodItem(name: "Food", imageFileName: "food.png", caloriesPerServing: 100, notificationScheduled: false)
         foodItems.append(foodItem)
         tableView.reloadData()
     }
@@ -30,11 +30,11 @@ class MainTableViewController: UITableViewController {
     }
     
     func initializeFoodItems() {
-        var foodItem = FoodItem(name: "Pizza", imageFileName: "pizza.png", caloriesPerServing: 300)
+        var foodItem = FoodItem(name: "Pizza", imageFileName: "pizza.png", caloriesPerServing: 300, notificationScheduled: false)
         foodItems.append(foodItem)
-        foodItem = FoodItem(name: "Spaghetti", imageFileName: "spaghetti.png", caloriesPerServing: 200)
+        foodItem = FoodItem(name: "Spaghetti", imageFileName: "spaghetti.png", caloriesPerServing: 200, notificationScheduled: false)
         foodItems.append(foodItem)
-        foodItem = FoodItem(name: "Ice Cream", imageFileName: "icecream.png", caloriesPerServing: 500)
+        foodItem = FoodItem(name: "Ice Cream", imageFileName: "icecream.png", caloriesPerServing: 500, notificationScheduled: false)
         foodItems.append(foodItem)
     }
 
@@ -75,6 +75,62 @@ class MainTableViewController: UITableViewController {
             foodItems.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! FoodCell
+        
+        // Check if notifications are enabled
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings(completionHandler: { (settings) in
+            if settings.alertSetting == .enabled {
+                self.displaySchedulingAlert(cell: cell)
+            } else {
+                print("Nothing is supposed to happen here lol")
+                self.displaySchedulingAlert(cell: cell)
+            }
+        })
+        
+        // If yes, then generate an alert asking the user if they would like to schedule a notification
+        // Title: "Food Notification", message: Do you want to schedule a notification for <food item> with yes or no as choices
+        // If no, nothing happens. If yes, item becomes red, notification scheduled for 5 seconds from now
+    }
+    
+    func displaySchedulingAlert(cell: FoodCell) {
+        DispatchQueue.main.async {
+            let notificationAlert = UIAlertController(title: "Food Notification", message: "Do you want to schedule a notification for the food " + cell.foodNameLabel.text!, preferredStyle: .alert)
+            
+            notificationAlert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action: UIAlertAction!) in
+                cell.backgroundColor = UIColor.red
+                self.scheduleNotification()
+            }))
+            
+            notificationAlert.addAction(UIAlertAction(title: "No", style: .default, handler: { (action: UIAlertAction!) in
+                print("Do nothing lol")
+            }))
+            
+            self.present(notificationAlert, animated: true, completion: nil)
+        }
+    }
+    
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Food Notification"
+        content.body = "Time to eat the food"
+        content.userInfo["message"] = "Yo!"
+        // Configure trigger for 5 seconds from now
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0,
+        repeats: false)
+        // Create request
+        let request = UNNotificationRequest(identifier: "NowPlusFive",
+        content: content, trigger: trigger)
+        // Schedule request
+        let center = UNUserNotificationCenter.current()
+        center.add(request, withCompletionHandler: { (error) in
+            if let err = error {
+                print(err.localizedDescription)
+            }
+        })
     }
 
     /*
