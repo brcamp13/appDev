68e4b8e3e115c145a06c7783cf5d36b51b6a99c4
brcamp13 <brandon.camp.bell@outlook.com>
2020-03-03 11:27:45 -0800

    Implement basically everything except for one error, entirely untested

--- FoodApp 2/FoodApp/AppDelegate.swift
+++ FoodApp 2/FoodApp/AppDelegate.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
+        
+        // Ask the user if they want notifications, the app should function properly regardless of the answer
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+            // Enable or disable features based on authorization.
+            print("The app should work regardless of what you choose lol")
+        }
+        
         return true
     }
 
@@ -32,6 +39,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("user responded to notification")
+        // Do stuff with response here (non-blocking)
+        let navVC = UIApplication.shared.windows.first!.rootViewController as!
+        UINavigationController
+        if let mainVC = navVC.topViewController as? ViewController {
+            mainVC.handleNotification(response)
+        }
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("received notification while in foreground; display?")
+        completionHandler([.alert]) // no options ([]) means no notification
+    }
 
 }
 
