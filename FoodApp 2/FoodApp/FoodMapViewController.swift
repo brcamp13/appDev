607f4ed83756765f780a9a144bff36617c735313
brcamp13 <brandon.camp.bell@outlook.com>
2020-03-31 16:49:32 -0700

    Remove food map code, add starting recipe code

--- FoodApp 2/FoodApp/FoodMapViewController.swift
+++ FoodApp 2/FoodApp/FoodMapViewController.swift
@@ -10,113 +10,14 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class FoodMapViewController: UIViewController, CLLocationManagerDelegate {
+class FoodMapViewController: UIViewController {
     
-    var locationManager = CLLocationManager()
     var labelName:String! = nil
     
-    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var mapLabel: UILabel!
+    @IBOutlet weak var foodImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapLabel.text = labelName
-        mapView.showsUserLocation = true
-        mapView.userTrackingMode = .follow
-        
-        //Zoom to user location
-        if let userLocation = locationManager.location?.coordinate {
-            let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 1000, longitudinalMeters: 1000)
-            mapView.setRegion(viewRegion, animated: false)
-        }
-        
-        initializeLocation()
-        
-        // Find the food
-        findFood()
     }
-    
-   /*
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    func initializeLocation() { // called from start up method
-        locationManager.delegate = self
-        let status = CLLocationManager.authorizationStatus()
-        switch status {
-            case .authorizedAlways, .authorizedWhenInUse:
-                startLocation()
-            case .denied, .restricted:
-                print("location not authorized")
-            case .notDetermined:
-                locationManager.requestWhenInUseAuthorization()
-        @unknown default:
-            print("IDK")
-        }
-    }
-    
-    // Delegate method called whenever location authorization status changes
-    func locationManager(_ manager: CLLocationManager,
-        didChangeAuthorization status: CLAuthorizationStatus) {
-        if ((status == .authorizedAlways) || (status == .authorizedWhenInUse)) {
-            self.startLocation()
-        } else {
-            self.stopLocation()
-        }
-    }
-    
-    func startLocation () {
-        locationManager.distanceFilter = kCLDistanceFilterNone
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.startUpdatingLocation()
-    }
-    
-    func stopLocation () {
-        locationManager.stopUpdatingLocation()
-    }
-    
-    // Delegate method called when location changes
-    func locationManager(_ manager: CLLocationManager,
-        didUpdateLocations locations: [CLLocation]) {
-        let location = locations.last
-        if let latitude = location?.coordinate.latitude {
-            print("Latitude: \(latitude)")
-        }
-        if let longitude = location?.coordinate.longitude {
-            print("Longitude: \(longitude)")
-        }
-    }
-    
-    // Delegate method called if location unavailable (recommended)
-    func locationManager(_ manager: CLLocationManager,
-        didFailWithError error: Error) {
-        print("locationManager error: \(error.localizedDescription)")
-    }
-    
-    func findFood() {
-        let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = self.labelName
-        request.region = mapView.region
-        let search = MKLocalSearch(request: request)
-        search.start(completionHandler: searchHandler)
-    }
-    
-    func searchHandler (response: MKLocalSearch.Response?, error: Error?) {
-        if let err = error {
-            print("Error occured in search: \(err.localizedDescription)")
-        } else if let resp = response {
-            print("\(resp.mapItems.count) matches found")
-            self.mapView.removeAnnotations(self.mapView.annotations)
-            for item in resp.mapItems {
-                let annotation = MKPointAnnotation()
-                annotation.coordinate = item.placemark.coordinate
-                annotation.title = item.name
-                self.mapView.addAnnotation(annotation)
-            }
-        }
-    }
-    
 }
