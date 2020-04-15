0075268e797423935b4c6058a873151ff46f5ec3
brcamp13 <brandon.camp.bell@outlook.com>
2020-04-14 17:30:29 -0700

    Implement everything except food images and actually testing the thing lol

--- secondFoodApp/FoodApp/FoodApp/TableViewController.swift
+++ secondFoodApp/FoodApp/FoodApp/TableViewController.swift
@@ -7,28 +7,44 @@
 //
 
 import UIKit
+import CoreData
+
 
 class TableViewController: UITableViewController {
     
-    var foods = [(FoodItem, Int)]()
-    var sortedFoods = [(FoodItem, Int)]()
+    var foods = [(FoodModel, Int)]()
     var genericFoodName: String?
+    
+    var foodOptions = [("chicken", "chicken.jpg"), ("sushi", "sushi.jpg"), ("pasta", "pasta.jpg"), ("lettuce", "lettuce.jpg"), ("ice cream", "iceCream.jpg")]
+    
+    var managedObjectContext: NSManagedObjectContext!
+    var appDelegate: AppDelegate!
     
     @IBOutlet weak var addButton: UIBarButtonItem!
     
     @IBAction func addTapped(_ sender: UIBarButtonItem) {
-        // Add generic food item to the foods array.
-        let genericFood = FoodItem(name: genericFoodName!, imageName: "genericFood.jpg", calories: 690)
-        self.foods.append((genericFood, foods.count))
-        processItems()
+        
+        // Get the random food from list of 5 w/corresponding image name
+        let foodIndex = Int.random(in: 0 ... 4)
+        let foodName = foodOptions[foodIndex].0
+        let imageFileName = foodOptions[foodIndex].1
+        
+        // Get the random calorie count
+        let calorieCount = Int.random(in: 100 ... 500)
+        
+        // Insert into coredata
+        addFood(name: foodName, imageFileName: imageFileName, calories: calorieCount)
+        
+        // Update foods array
+        updateFoodArray()
         tableView.reloadData()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        appDelegate = UIApplication.shared.delegate as? AppDelegate
+        managedObjectContext = appDelegate.persistentContainer.viewContext
         initialize()
-        processItems()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -39,32 +55,50 @@ class TableViewController: UITableViewController {
         // Initialize an array of three food items, all different.
     }
     
+    // Clear the food array and update the foods based on what's in coredata
+    func updateFoodArray() {
+        self.foods.removeAll()
+        let updatedFoods = getFood() as! [FoodModel]
+        var index = 0
+        for food in updatedFoods {
+            self.foods.append((food, index))
+            index = index + 1
+        }
+    }
+    
     func initialize() {
-        // Add the starting food items to the food array.
-        let pasta = FoodItem(name: "Pasta", imageName: "pasta.jpg", calories: 500)
-        let sushi = FoodItem(name: "Sushi", imageName: "sushi.jpg", calories: 150)
-        let chicken = FoodItem(name: "Chicken", imageName: "chicken.jpg", calories: 250)
-        self.foods.append((pasta, foods.count))
-        self.foods.append((sushi, foods.count))
-        self.foods.append((chicken, foods.count))
-        
+        // self.foods.append((pasta, foods.count))
         tableView.rowHeight = 58
+    }
+    
+    func removeFood(_ food: FoodModel) {
+        managedObjectContext.delete(food)
+        appDelegate.saveContext()
+    }
+    
+    func addFood(name: String, imageFileName: String, calories: Int) {
+        let food = NSEntityDescription.insertNewObject(forEntityName:
+        "Food", into: self.managedObjectContext)
+        food.setValue(name, forKey: "name")
+        food.setValue(imageFileName, forKey: "imageFileName")
+        food.setValue(calories, forKey: "calories")
+        appDelegate.saveContext() // In AppDelegate.swift
+    }
+    
+    func getFood() -> [NSManagedObject] {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Food")
+        var food: [NSManagedObject] = []
+        do {
+            food = try self.managedObjectContext.fetch(fetchRequest)
+        } catch {
+            print("getFood error: \(error)")
+        }
+        return food
     }
 
     // MARK: - Table view data source
     
-    func processItems() {
-        if UserDefaults.standard.bool(forKey: "sortItems") {
-            sortedFoods = foods.sorted(by: {$0.0.name < $1.0.name})
-        } else{
-            sortedFoods = foods
-        }
-        
-        genericFoodName = UserDefaults.standard.object(forKey: "genericFood") as? String
-    }
-    
     override func viewDidAppear(_ animated: Bool) {
-        processItems()
         tableView.reloadData()
     }
 
@@ -75,7 +109,7 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return self.sortedFoods.count
+        return self.foods.count
     }
 
 
@@ -83,9 +117,9 @@ class TableViewController: UITableViewController {
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "foodCell", for: indexPath) as! FoodCell
         let indexRow = indexPath.row
-        cell.foodNameLabel.text = self.sortedFoods[indexRow].0.name
-        cell.foodImageView.image = UIImage(named: self.sortedFoods[indexRow].0.imageName)
-        cell.calorieLabel.text = "\(self.sortedFoods[indexRow].0.calories) cals"
+        cell.foodNameLabel.text = self.foods[indexRow].0.name
+        cell.foodImageView.image = UIImage(named: self.foods[indexRow].0.imageFileName!)
+        cell.calorieLabel.text = "\(self.foods[indexRow].0.calories) cals"
         return cell
     }
 
@@ -100,10 +134,13 @@ class TableViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            // Delete the food from core data, call the func that updates array from core data
             // Delete the row from the data source first
             let row = indexPath.row
-            self.sortedFoods.remove(at: row)
-            self.foods = self.foods.filter {$0.1 != row}
+            let food = self.foods[row]
+            self.foods.remove(at: row)
+            self.removeFood(food.0)
+            self.updateFoodArray()
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
