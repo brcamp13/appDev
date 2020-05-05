d1e32191f214a6c71f66cb5bbf4c43e659e2bb90
brcamp13 <brandon.camp.bell@outlook.com>
2020-05-04 20:05:46 -0700

    Add basic article detail view

---
+++ FoodApp 2/FinalNewsApp/ArticleDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ArticleDetailViewController.swift
+//  FinalNewsApp
+//
+//  Created by Brandon Campbell on 5/4/20.
+//  Copyright © 2020 Washington State University. All rights reserved.
+//
+
+import UIKit
+
+class ArticleDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var articleImageView: UIImageView!
+    
+    @IBOutlet weak var articleContentView: UILabel!
+    @IBOutlet weak var moreButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
